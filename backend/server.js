95c1405f0cf4185677588c90c43b133fd23764e7
Karan Sayambar <karansayambar@gmail.com>
2024-09-04 17:34:43 +0530

    code added

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,14 @@ const cors = require('cors')
 const db = require("./db"); // Assuming this connects to your MongoDB
 const authRouter = require("./routes/authRouter");
 const todoRouter = require("./routes/todoRouter");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const BASE_URL = process.env.BASE_URL;
 // Constants
 const app = express();
 const PORT = process.env.PORT || 3000; // Default to 3000 if PORT is not defined
+
+
 const store = new MongoDBStore({
   uri: process.env.DB_URL,
   collection: "sessions", // Correct collection name
@@ -23,6 +26,7 @@ const store = new MongoDBStore({
 
 // Middlewares
 app.use(express.json()); // To parse JSON bodies
+app.use(cookieParser())
 app.use(cors({
   origin : `${BASE_URL}`, //Frontend URL
   credentials : true // Allow credentials (cookies, headers)
